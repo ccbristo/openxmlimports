24a235a417ac9dad725a9fc0694ea501d17b7abe
Chris Bristol <devnull@localhost>
2012-12-20 16:35:37 -0500

    Simplified CamelCaseNamingConvention to use Regexes.

--- ExcelImports/CamelCaseNamingConvention.cs
+++ ExcelImports/CamelCaseNamingConvention.cs
@@ -1,56 +1,18 @@
 ﻿using System.Reflection;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ExcelImports
 {
     public class CamelCaseNamingConvention : INamingConvention
     {
+        private static readonly Regex UpperCaseFollowedByUpperLower = new Regex(@"(\P{Ll})(\P{Ll}\p{Ll})", RegexOptions.Compiled);
+        private static readonly Regex LowerCaseFollowedByNonLower = new Regex(@"(\p{Ll})(\P{Ll})", RegexOptions.Compiled);
+
         public string GetName(MemberInfo member)
         {
-            var sb = new StringBuilder(member.Name);
-
-            bool lastWasUpper = char.IsUpper(member.Name[0]);
-
-            for (int i = 1; i < sb.Length - 1; i++)
-            {
-                char current = sb[i];
-                char next = sb[i + 1];
-
-                if (IsUpperToLowerCaseTransition(current, next))
-                {
-                    sb.Insert(i, " ");
-                    i++; // length goes up, keep our pointer to the right
-                }
-                else if (IsLowerToUpperCaseTransition(current, next) ||
-                    IsAlphaToNumericTransition(current, next) ||
-                    IsNumericToAlphaTransition(current, next))
-                {
-                    sb.Insert(i + 1, " ");
-                    i += 2; // move to the right of the space we just inserted
-                }
-            }
-
-            return sb.ToString();
-        }
-
-        private static bool IsLowerToUpperCaseTransition(char current, char next)
-        {
-            return char.IsLower(current) && char.IsUpper(next);
-        }
-
-        private static bool IsUpperToLowerCaseTransition(char current, char next)
-        {
-            return char.IsUpper(current) && char.IsLower(next);
-        }
-
-        private static bool IsAlphaToNumericTransition(char current, char next)
-        {
-            return char.IsLetter(current) && char.IsDigit(next);
-        }
-
-        private static bool IsNumericToAlphaTransition(char current, char next)
-        {
-            return char.IsDigit(current) && char.IsLetter(next);
+            string r1 = UpperCaseFollowedByUpperLower.Replace(member.Name, "$1 $2");
+            string r2 = LowerCaseFollowedByNonLower.Replace(r1, "$1 $2");
+            return r2;
         }
     }
 }
